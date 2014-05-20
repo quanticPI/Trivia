f27c8db5cb8e3ad465f9844aea3e2d2eb819025a
Nikolay <nauzunov@gmail.com>
2014-05-20 14:20:26 +0200

    acabar con fabrica y decorador

---
+++ src/logica/Trivia.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class Trivia {
+
+	public static void main(String[] args) {
+		FabricaJuego fabrica = new FabricaConcreta();
+		Juego game = fabrica.metodoFabrica(Juego.HISTORIA);
+		
+		IPregunta preg = new PreguntaDificil(new PreguntaFacil(new PreguntaBasica()));
+		preg.setTextoPregunta("asda");
+		System.out.println(preg.getTextoPregunta());;
+		game.setPregunta(preg, 0);
+		System.out.println(game.getPregunta(0).getRespuestaCorrecta());
+	}
+
+}
