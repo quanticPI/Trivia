b27dca5cc15a79fddff3a425cbd3d75e0695d019
Maria <mamargu7@gmail.com>
2014-06-08 12:02:26 +0200

    Subiendo Puntuacion

--- src/presentacion/puntuacion.java
+++ src/presentacion/puntuacion.java
@@ -4,20 +4,29 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+
+import excepciones.*;
+import logica.*;
 
 public class puntuacion extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTable table;
+	private Controlador control;
+	private JTable tablePuntuacion;
 
 	/**
 	 * Launch the application.
@@ -32,66 +41,86 @@ public class puntuacion extends JDialog {
 		}
 	}
 
-	/**
-	 * Create the dialog.
-	 */
-	public puntuacion() {
-		setTitle("Puntuaci\u00F3n");
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+	
+	public puntuacion() throws LogicaExcepcion, DAOExcepcion{
+		setBounds(130, 130, 760, 348);	
+		setTitle("Consultar Puntuacion");
+		control = Controlador.getInstance();
+		getContentPane().setLayout(null);
 		
-		table = new JTable();
-		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
-		gl_contentPanel.setHorizontalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addGap(15)
-					.addComponent(table, GroupLayout.PREFERRED_SIZE, 409, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(16, Short.MAX_VALUE))
-		);
-		gl_contentPanel.setVerticalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(table, GroupLayout.DEFAULT_SIZE, 217, Short.MAX_VALUE)
-					.addContainerGap())
-		);
-		contentPanel.setLayout(gl_contentPanel);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton atrasButton = new JButton("Atr\u00E1s");
-				atrasButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						dispose();
-					}
-				});
-				buttonPane.add(atrasButton);
-				getRootPane().setDefaultButton(atrasButton);
-			}
-			{
-				JButton avanzadoButton = new JButton("Avanzado");
-				avanzadoButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent evt) {
-						avanzadoActionPerformed(evt);
-					}
-				});
-				buttonPane.add(avanzadoButton);
-			}
-		}
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 6, 749, 314);
+		getContentPane().add(scrollPane);
+		
+		tablePuntuacion = new JTable(new PuntuacionTableModel());
+		scrollPane.setViewportView(tablePuntuacion);
 	}
 	
-	public void avanzadoActionPerformed(ActionEvent evt){
+	public void cargaPuntuacion(){ 
 		try{
-			puntuacionAvanzada pu = new puntuacionAvanzada(); 
-			pu.setVisible(true);		
-		}catch (Exception e){
-			JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+			List<Jugador> listaJugador= control.encontrarJugadores(); 
+			Iterator<Jugador>it= listaJugador.iterator();
+			Jugador ju;
+			PuntuacionTableModel model = (PuntuacionTableModel) tablePuntuacion.getModel(); 
+			model.clear();
+			while (it.hasNext()){
+				ju=it.next(); 
+				model.addRow(ju);
+			}
 		}
+		catch (Exception e){
+			JOptionPane.showMessageDialog(this,e.getMessage(),"ERROR", JOptionPane.ERROR_MESSAGE);
+		} 
 	}
+	
+	
+	class PuntuacionTableModel extends AbstractTableModel {
+		
+		private static final long serialVersionUID = 1L;
+		private String[] columnNames = { "Usuario", "Puntuaci—n"};
+		private ArrayList<Jugador> data = new ArrayList<Jugador>();
+		
+		public int getColumnCount(){ 
+			return columnNames.length;
+		}
+		
+		public int getRowCount(){ 
+			return data.size();
+		}
+		
+		public String getColumnName(int col){
+			return columnNames[col]; 
+		}
+		
+		public Object getValueAt(int row, int col){ 
+			Jugador ju = data.get(row);
+			switch(col){
+				case 0: return ju.getNombre();
+				case 1: return ju.getPuntos();
+				default: return null;
+			}
+		}
+		
+		public void clear(){ 
+			data.clear();
+		}
+		
+		public Class<? extends Object> getColumnClass(int c){ 
+			return getValueAt(0, c).getClass();
+		}
+		
+		public void addRow(Jugador row){
+			data.add(row); 
+			this.fireTableDataChanged();
+		}
+		
+		public void delRow(int row){
+			data.remove(row); 
+			this.fireTableDataChanged();
+		} 
+	}
+	
+	
+	
 	
 }
