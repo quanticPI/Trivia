3e84a060b140ec421968bbe6c376416f8e597919
sasiro <pablopalomares5@hotmail.com>
2014-06-09 11:42:28 +0200

    sql

---
+++ src/persistencia/tablas.sql
@@ -0,0 +1,59 @@
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(11,'CINE','Tobey Maguire','Kristen Dunst','James Franco','Rosemary Harris','Actor Principal de la pelicula "Spiderman"')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(12,'CINE','Tom Cruise','Kelly McGillis','Robert Pattinson','Ben Stiler','Actor principal de la pelicula "Top Gun"')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(13,'CINE','Mario Casas','Robert Pattinson','Adam Sandler','Brad Pitt','Actor pricipal de la pelicula "Tres Metros Sobre el Cielo"')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(14,'CINE','Brad Pitt','Tom Cruise','Bruce Willis','James Franco','Actor principal de la pelicula "El Curioso Caso De Benjamin Button"')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(15,'CINE','Charlize Theron','katie Holmes','Cameron Diaz','Julia Roberts','Actriz principal de la pelicula "Joe"')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(16,'CINE','Charlize Theron','katie Holmes','Cameron Diaz','Julia Roberts','Actriz principal de la pelicula "Joe"')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(17,'CINE','Thomas Cruise Mapother I','Thomas Andres Cruise','Tom Cruise Mapother','Thomas Cruise Mapother IV','Cual es el verdadero nombre de Tom Cruise?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(18,'CINE','James Edward Franco','James luis Franco Rojas',' Franco James','James Franco','Cual es el verdadero nombre de James Franco?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(19,'CINE','Cameron Michelle Diaz','Cameron Antonia Diaz',' Cameron Pascal Diaz',' Cameron Diaz Diaz','Cual es el verdadero nombre de Cameron Diaz?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(20,'CINE','Willard Christopher Smith Jr','Will Smith JP',' Willard JP Smith','Will Smith Tj','Cual es el verdadero nombre de Will Simth')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(21,'HISTORIA','Los bárbaros','los chinos','los griegos',' los egipcios','¿Qué pueblos invadieron Roma?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(22,'HISTORIA','476','311','1492','1000','¿En qué año cayó Roma?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(23,'HISTORIA','Los reyes y nobleza',' los campesinos','artesanos','el clero','¿Qué parte de la sociedad recibía vasallaje?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(24,'HISTORIA','Atila','Tom Cruise',' Boabdil','Centurión','¿Quién era el rey de los hunos?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(25,'HISTORIA','1492','1515',' 1688','1496','¿En qué año termina la Edad Media?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(26,'HISTORIA','Alta y Baja Edad Media','Gran y pequeña Edad Media','No tiene divisiones, es un período muy amplio','Primera y segunda Edad Media','¿En qué dos grandes etapas se divide la Edad Media?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(27,'HISTORIA','La ruta de la seda','La ruta de la plata','El camino de Santiago','La ruta africana','En la Edad Media el comercio europeo con Oriente se hacía a través de:')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(28,'HISTORIA','Portugal','Francia','Italia','España','¿De qué país fueron los primeros navegantes en aventurarse hacia el Sur por el Atlántico?')
+
+insert into PREGUNTA  (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(29,'HISTORIA','Bartolome Dias','Magallanes','Vasco da Gama','Cristóbal Colón','¿Qué navegante portugués llegó hasta el cabo de Buena Esperanza o de las Tormentas en 1488?')
+
+insert into PREGUNTA (ID,CATEGORIA,RESPUESTA_CORRECTA,RESPUESTA_FALSA1,RESPUESTA_FALSA2,RESPUESTA_FALSA3,TEXTO_PREGUNTA)values 
+(30,'HISTORIA','Calicut',' Azores',' Madeira','Trinidad','¿Qué puerto de la India que abrió el camino hacia las islas Molucas alcanzó el navegante portugués Vasco da Gama en el año 1498?')
